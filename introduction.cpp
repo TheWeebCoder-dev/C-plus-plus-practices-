5dd74b7b81fe8f720e0eda66f46e98dcf3ecfe60
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 11:41:26 -0600

    Started with a normal practice

---
+++ introduction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int Square(int);
+int Circle(int);
+
+int main(){
+    cout << "The square is" <<Square(27) <<endl;
+    cout << "The cirlce is" <<Circle(30) <<endl;
+
+    
+}
