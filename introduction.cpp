20734ea41ce41699285381e8cb08a4da5bf57964
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 11:55:46 -0600

    Played with some functions

--- introduction.cpp
+++ introduction.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 int Square(int);
 int Circle(int);
+double Andere(double);
 
 int main(){
     cout << "The square is" <<Square(27) <<endl;
     cout << "The cirlce is" <<Circle(30) <<endl;
+    cout << "The special ecuation result is" <<Andere(2.50) <<endl;
 
-    
+    return 0;
 }
+
+int Square (int n){
+    return n * n;
+}
+int Circle(int c){
+    return c*c*c;
+}
+
+double Andere(double r){
+    return r*r;
+}
+
