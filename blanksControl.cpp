cda55fed6ea1d96aa791e7b377a022da4b335c58
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 13:05:12 -0600

    tried out the iomanip library and manipulated the blanks of the info that was specified

---
+++ blanksControl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int ans = 72;
+    int num = 7132;
+
+    cout << setw(4) <<ans
+    <<setw(5) <<num
+    <<setw(4) <<"Hi";
+}
