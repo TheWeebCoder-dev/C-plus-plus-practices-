221a06a1a962bcd904adec8807c15e29e465aa96
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 21:23:19 -0600

    lenght function tried

---
+++ stringExtraFunctions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+int main(){
+    string userInput;
+
+    cout <<"enter some input a word with minimum 4 characters" <<endl;
+    cin >> userInput;
+    cout << "processing different functiond for a string" << endl;
+    cout <<userInput.length() <<endl;
+}
