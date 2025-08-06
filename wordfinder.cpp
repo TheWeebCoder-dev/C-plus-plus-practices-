303bdbb2510b4b47625c0de885f21fa94a14d2b5
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 22:06:33 -0600

    added a cutting characters of a string that was entered function

--- wordfinder.cpp
+++ wordfinder.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 string sentence;
 string word;
+int eliminationOfCharacters;
+
 
 int main(){
 cout <<"please enter a sentence" <<endl;
@@ -12,5 +14,9 @@ cout << "now enter a word that you would like to find according to that sentence
 cin >> word;
 cout << "the lenght of that sentence is " <<sentence.length() <<endl;
 cout << "and the position of the word you are searching for is " <<sentence.find(word) <<endl;
+cout << "now based of the word position we will mark the beggining and ending of the word"<<endl;
+cout << "now enter which number of characters you want to cut word you want to cut" <<endl;
+cin >> eliminationOfCharacters;
+cout << "the result is: " <<word.substr(eliminationOfCharacters) <<endl;
 cout <<"have a nice day user" <<endl;
 }
