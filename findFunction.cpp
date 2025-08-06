f4881179faff9d3e8068e9387e09bd0b7c773ff2
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-05 21:29:09 -0600

    experimented with the find function

---
+++ findFunction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string phrase = "Fun programming";
+
+//the find function intialiazeses from zero to the number that is requiered
+
+int main(){
+    
+cout << phrase.find("g") <<endl;
+}
