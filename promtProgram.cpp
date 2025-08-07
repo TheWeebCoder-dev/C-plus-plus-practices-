3a14536c0eaf40bd3ef3f2922dd10ea8eb1a617a
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-06 18:53:42 -0600

    finished the quantity, part and price prompt

--- promtProgram.cpp
+++ promtProgram.cpp
@@ -9,7 +9,20 @@ int main(){
     double unitPrice;
     double totalPrice;
 
-    cout << fixed << showpoint <<setprecision(2);
-
+    cout << fixed << showpoint << setprecision(2);
+    cout << "Enter the part number: " << endl;
+    cin >> partNumber;
+    cout <<"Enter the quantity of this part ordered " <<endl;
+    cin >> quantity;
+    cout << "Enter the unit price for this part" <<endl;
+    cin >> unitPrice;
     
+    totalPrice = quantity * unitPrice;
+    cout << "Part " <<partNumber 
+    <<", quantity " <<quantity
+    <<", at $ " <<unitPrice << " each" <<endl;
+    cout <<"totals: " <<totalPrice <<endl;
+    return 0;
+
+
 }
