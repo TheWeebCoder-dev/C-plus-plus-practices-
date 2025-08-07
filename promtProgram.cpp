1532f22a39e950e7f698e51cb5c8a5c394ef25cc
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-06 18:32:04 -0600

    making a prompt

---
+++ promtProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+    int partNumber;
+    int quantity;
+    double unitPrice;
+    double totalPrice;
+
+    cout << fixed << showpoint <<setprecision(2);
+
+    
+}
