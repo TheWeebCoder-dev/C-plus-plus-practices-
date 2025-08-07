a2df01d914b93044698fe30e99169dc095fcd3a1
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-07 11:14:21 -0600

    improved the program

--- changingLetters.cpp
+++ changingLetters.cpp
@@ -10,26 +10,26 @@ int userFunctionSelection;
 void Capitalize(string data){
     string capitalizedData;
 
-    for(int i;i<data.length();++i){
-capitalizedData = toupper(data[i]) ;   
+    for(int i=0;i<data.length();++i){
+capitalizedData += toupper(data[i]) ;   
 }
-    cout << "here is your capitalized word" <<capitalizedData;
+    cout << "here is your capitalized word " <<capitalizedData;
 }
 
 void Lower(string data){
     string lowercasedData;
-    for (int i; i < data.length(); i++)
+    for (int i=0; i < data.length(); i++)
     {
-     lowercasedData = tolower(data[i]);
+     lowercasedData += tolower(data[i]);
     }
-   cout << "here is your lowedcase word" <<lowercasedData;
+   cout << "here is your lowedcase word " <<lowercasedData;
     
 }
 
 int main(){
-    cout << "Pleace enter a word" <<endl;
+    cout << "Please enter a word" <<endl;
     cin >> userInput;
-    cout << "Select an option: 1 to capitalize your word or 2 to lowercase your word" <<endl;
+    cout << "Select an option: 1 to capitalize your word or 2 to lowercase your word " <<endl;
     cin >> userFunctionSelection;
 switch (userFunctionSelection)
 {
@@ -38,10 +38,14 @@ case 1:
     break;
 
     case 2:
-    Lower(userInput);
-default:
+    Lower (userInput);
     break;
+default:
+cout << "Please enter a word that is correct" << endl;
+    break;
+    return 0;
 }
+return 0;
 
 
 }
